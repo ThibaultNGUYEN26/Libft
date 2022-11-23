a7d5557f2d168fb297524d1ddcc83eff68c8397e
Thibault <118262807+ThibaultNGUYEN26@users.noreply.github.com>
2022-11-23 15:15:17 +0100

    Add files via upload

---
+++ libft/ft_substr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 15:14:14 by thibnguy          #+#    #+#             */
+/*   Updated: 2022/11/22 17:11:34 by thibnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*s1;
+
+	if (!s)
+		return (NULL);
+	s1 = malloc(sizeof(char) * (len + 1));
+	if (!s1)
+		return (NULL);
+	i = 0;
+	while ((i < len) && (start <= ft_strlen(s)))
+	{
+		s1[i] = s[start + i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+} 
