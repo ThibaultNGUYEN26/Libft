e96f30ed3e6d9c6c3669d337ca67e2b6a741353f
Thibault <118262807+ThibaultNGUYEN26@users.noreply.github.com>
2022-11-28 18:19:53 +0100

    Add files via upload

---
+++ libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thibnguy <thibnguy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 16:17:18 by thibnguy          #+#    #+#             */
+/*   Updated: 2022/11/28 18:16:00 by thibnguy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
